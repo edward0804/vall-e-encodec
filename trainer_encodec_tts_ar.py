688edb7299526ff3bbee9bed6a9b35f860e36f39
“kuan2jiu99” <b08901216@ntu.edu.tw>
2023-05-07 12:15:32 +0800

    update encodec fine-tune

--- trainer_encodec_tts_ar.py
+++ trainer_encodec_tts_ar.py
@@ -5,13 +5,15 @@ from transformers import (AutoTokenizer, BartForConditionalGeneration,
                           Seq2SeqTrainingArguments, GenerationConfig)
 
 import wandb
-
+from encodec_bart_model import BartEncodecForConditionalGeneration
 
 # Load dataset and tokenizer
-train_dataset = load_dataset("voidful/librispeech_encodec", split="trainclean100+trainclean360+trainother500")
-valid_dataset = load_dataset("voidful/librispeech_encodec", split="validationclean")
+# train_dataset = load_dataset("voidful/librispeech_encodec", split="trainclean100+trainclean360+trainother500")
+# lca0503/soxdata_small_encodec
+train_dataset = load_dataset("lca0503/soxdata_small_encodec")
+# valid_dataset = load_dataset("voidful/librispeech_encodec", split="validationclean")
+valid_dataset = load_dataset("voidful/librispeech_encodec")
 tokenizer = AutoTokenizer.from_pretrained("voidful/bart-base-unit")
-
 # Load model
 model = BartForConditionalGeneration.from_pretrained("voidful/bart-base-unit")
 
@@ -31,7 +33,7 @@ training_args = Seq2SeqTrainingArguments(
     evaluation_strategy="steps",
     eval_steps=5000,
     predict_with_generate=True,
-    fp16=True,
+    # fp16=True,
     gradient_accumulation_steps=2,
     learning_rate=1e-4,
     generation_max_length=1024,
@@ -39,13 +41,20 @@ training_args = Seq2SeqTrainingArguments(
 
 # Filter long examples
 def filter_examples(example):
-    return len(example[f"encodec_0"]) <= 1000
+    # return len(example[f"encodec_0"]) <= 1000
+    return len(example[f"src_encodec_0"]) <= 1000 and len(example[f"tgt_encodec_0"]) <= 1000
 
 train_dataset = train_dataset.filter(filter_examples)
-valid_dataset = valid_dataset.filter(filter_examples)
+# valid_dataset = valid_dataset.filter(filter_examples)
 
 def pad_sequences(sequences, max_length, padding_value):
     return [sequence + [padding_value] * (max_length - len(sequence)) for sequence in sequences]
+
+def pad_sequences_for_input_id(sequences, max_length, padding_value):
+    return sequences + [padding_value] * (max_length - len(sequences))
+
+def pad_sequences_for_attn_mask(sequences, max_length):
+    return sequences + [0] * (max_length - len(sequences)) 
 
 def process_data_to_model_inputs(batch):
     input_ids = []
@@ -55,7 +64,67 @@ def process_data_to_model_inputs(batch):
 
     max_length = 1023  # You can set this to a suitable value based on your dataset
 
-    for b in range(len(batch['text'])):
+    for b in range(len(batch["instruction"])):
+        # first layer AR data
+        # data = tokenizer(batch["text"][b], padding='max_length', truncation=True, max_length=max_length)
+        # encode_input = tokenizer.convert_tokens_to_ids([f"v_tok_{u}" for u in batch[f'encodec_{0}'][b]])
+        # decoder_input_id = [tokenizer.bos_token_id] + encode_input
+        # label = encode_input + [tokenizer.eos_token_id]
+        # input_ids.append(data['input_ids'])
+        # attention_mask.append(data['attention_mask'])
+        # decoder_input_ids.append(decoder_input_id)
+        # labels.append(label)
+        # print(batch["file_id"][b])
+        batch_data = batch["instruction"][b] + " </s> " + batch["transcription"][b]
+        # data = tokenizer(batch_data, padding='max_length', truncation=True, max_length=max_length)
+        data = tokenizer(batch_data)
+        # print(f"data: {data['input_ids']}")
+        # input of decoder.
+        encode_input = tokenizer.convert_tokens_to_ids([f"v_tok_{u}" for u in batch[f'src_encodec_0'][b]])
+        data['input_ids'] += encode_input
+        data['attention_mask'] += [1] * len(encode_input)
+        # print(f"data: {data['input_ids']}")
+        
+        # output of decoder.
+        encode_output = tokenizer.convert_tokens_to_ids([f"v_tok_{u}" for u in batch[f'tgt_encodec_0'][b]])
+
+        # source unit.
+        decoder_input_id = [tokenizer.bos_token_id] + encode_output
+
+        # target unit.
+        label = encode_output + [tokenizer.eos_token_id]
+        
+        # padding.
+        data['input_ids'] = pad_sequences_for_input_id(data['input_ids'], max_length=max_length, padding_value=tokenizer.pad_token_id)
+        data['attention_mask'] = pad_sequences_for_attn_mask(data['attention_mask'], max_length=max_length)
+        # print(data['attention_mask'])
+        # print(data['input_ids'])
+        # break
+        input_ids.append(data['input_ids'])
+        attention_mask.append(data['attention_mask'])
+        decoder_input_ids.append(decoder_input_id)
+        labels.append(label)
+
+    # Pad decoder_input_ids and labels
+    decoder_input_ids = pad_sequences(decoder_input_ids, max_length=max_length, padding_value=tokenizer.pad_token_id)
+    labels = pad_sequences(labels, max_length=max_length, padding_value=-100)
+
+    return {
+        "input_ids": input_ids,
+        "attention_mask": attention_mask,
+        "decoder_input_ids": decoder_input_ids,
+        "labels": labels
+    }
+
+def process_data_to_model_inputs_val(batch):
+    input_ids = []
+    attention_mask = []
+    decoder_input_ids = []
+    labels = []
+
+    max_length = 1023  # You can set this to a suitable value based on your dataset
+
+    for b in range(len(batch["text"])):
         # first layer AR data
         data = tokenizer(batch["text"][b], padding='max_length', truncation=True, max_length=max_length)
         encode_input = tokenizer.convert_tokens_to_ids([f"v_tok_{u}" for u in batch[f'encodec_{0}'][b]])
@@ -79,16 +148,21 @@ def process_data_to_model_inputs(batch):
 
 train_dataset = train_dataset.map(
     process_data_to_model_inputs,
-    remove_columns=train_dataset.column_names,
+    # remove_columns=train_dataset.column_names,
+    # remove_columns=train_dataset["train"].column_names,
+    remove_columns=["src_encodec_2", "src_encodec_3", "src_encodec_4", "src_encodec_5", "src_encodec_6", "src_encodec_7",
+    "src_encodec_1", "tgt_encodec_0", "tgt_encodec_1", "tgt_encodec_2", "tgt_encodec_3", "tgt_encodec_4", "tgt_encodec_5",
+    "tgt_encodec_6", "tgt_encodec_7", "file_id", "instruction", "transcription", "src_encodec_0"],
     batched=True,
     batch_size=training_args.per_device_train_batch_size
 )
-valid_dataset = valid_dataset.map(
-    process_data_to_model_inputs,
-    remove_columns=valid_dataset.column_names,
-    batched=True,
-    batch_size=training_args.per_device_eval_batch_size
-)
+print(train_dataset)
+# valid_dataset = valid_dataset.map(
+#     process_data_to_model_inputs_val,
+#     # remove_columns=valid_dataset.column_names,
+#     batched=True,
+#     batch_size=training_args.per_device_eval_batch_size
+# )
 
 # Define a data collator to handle tokenization
 data_collator = DataCollatorForSeq2Seq(tokenizer, model=model)
@@ -119,8 +193,8 @@ wandb.init(name)
 trainer = Seq2SeqTrainer(
     model=model,
     args=training_args,
-    train_dataset=train_dataset,
-    eval_dataset=valid_dataset,
+    train_dataset=train_dataset["train"],
+    # eval_dataset=valid_dataset,
     data_collator=data_collator,
     tokenizer=tokenizer,
     compute_metrics=compute_metrics,
--- trainer_encodec_tts_nar.py
+++ trainer_encodec_tts_nar.py
@@ -7,18 +7,18 @@ from transformers import (
     Seq2SeqTrainer,
     Seq2SeqTrainingArguments
 )
-from encodec_model.nar_bart_model import NARBartForConditionalGeneration
-
+from nar_bart import NARBartForConditionalGeneration
 
 name = 'libri960_nar_wr0.08_lr1e-4'
 wandb.init(name)
 
 # Load dataset and tokenizer
-train_dataset = load_dataset("voidful/librispeech_encodec", split="trainclean100+trainclean360+trainother500")
+# train_dataset = load_dataset("voidful/librispeech_encodec", split="trainclean100+trainclean360+trainother500")
+train_dataset = load_dataset("lca0503/soxdata_small_encodec")
 valid_dataset = load_dataset("voidful/librispeech_encodec", split="validationclean")
 tokenizer = AutoTokenizer.from_pretrained("voidful/bart-base-unit")
 model = NARBartForConditionalGeneration.from_pretrained("voidful/bart-base-unit")
-
+print(train_dataset)
 # Set training parameters
 training_args = Seq2SeqTrainingArguments(
     output_dir="./training_output",
@@ -33,7 +33,7 @@ training_args = Seq2SeqTrainingArguments(
     save_total_limit=2,
     evaluation_strategy="steps",
     eval_steps=10000,
-    fp16=True,
+    # fp16=True,
     gradient_accumulation_steps=2,
     learning_rate=1e-4,
 )
@@ -44,54 +44,102 @@ data_collator = DataCollatorForSeq2Seq(tokenizer, model=model)
 def pad_sequences(sequences, max_length, padding_value):
     return [sequence + [padding_value] * (max_length - len(sequence)) for sequence in sequences]
 
+def pad_sequences_for_input_id(sequences, max_length, padding_value):
+    return sequences + [padding_value] * (max_length - len(sequences))
+
+def pad_sequences_for_attn_mask(sequences, max_length):
+    return sequences + [0] * (max_length - len(sequences)) 
+
 # Define training and validation functions
 def process_data_to_model_inputs(batch):
     input_ids = []
+    prompt_input_ids = []
+    src_input_ids = []
     attention_mask = []
     decoder_input_ids = []
     labels = []
 
-    max_length = 1023  # You can set this to a suitable value based on your dataset
+    src_input_ids_ = []
+    attention_mask_ = []
 
-    for b in range(len(batch['text'])):
-        data = tokenizer(batch["text"][b], padding='max_length', truncation=True, max_length=max_length)
+    max_length = 1023  # You can set this to a suitable value based on your dataset
+    input_datas = []
+    for b in range(len(batch["instruction"])):
+        # print(f"batch: {batch['instruction'][b]}")
+        batch_data = batch["instruction"][b] + " </s> " + batch["transcription"][b]
+        prompt_data = tokenizer(batch_data, padding='max_length', truncation=True, max_length=max_length)
+        
+        data = tokenizer(batch_data, padding='max_length', truncation=True, max_length=max_length)
+
+        encodec_input = [] # source encodec input.
+
+        for i in range(0, 8):
+            decoder_input_id = tokenizer.convert_tokens_to_ids(
+                ([f"v_tok_{u + i * 1000}" for u in batch[f'src_encodec_{i}'][b]]))
+            
+            encodec_input.append(decoder_input_id)
+
+        input_datas.append(encodec_input)
+        
+        # Pad the input data sequences and create attention masks
+        padded_input_datas = []
+        attention_masks = []
+        for input_data in input_datas:
+            padded_input_data = []
+            for seq in input_data:
+                seq_len = len(seq)
+                padded_seq = seq + [tokenizer.pad_token_id] * (max_length - seq_len)
+                mask = [1] * seq_len + [0] * (max_length - seq_len)
+                padded_input_data.append(padded_seq)
+            padded_input_datas.append(padded_input_data)
+            attention_masks.append(mask)
+       
+        batch["input_ids"] = padded_input_datas
+        batch["attention_mask"] = attention_masks
+        
         for i in range(1, 8):
             decoder_input_id = tokenizer.convert_tokens_to_ids(
-                [f"v_tok_{u + (i - 1) * 1000}" for u in batch[f'encodec_{i - 1}'][b]])
-            label = tokenizer.convert_tokens_to_ids([f"v_tok_{u + i * 1000}" for u in batch[f'encodec_{i}'][b]])
-            input_ids.append(data['input_ids'])
-            attention_mask.append(data['attention_mask'])
+                [f"v_tok_{u + (i - 1) * 1000}" for u in batch[f'tgt_encodec_{i - 1}'][b]])
+            
+            label = tokenizer.convert_tokens_to_ids([f"v_tok_{u + i * 1000}" for u in batch[f'tgt_encodec_{i}'][b]])
+
+            # print(f"decoder_input_id: {len(decoder_input_id)}")
+
             decoder_input_ids.append(decoder_input_id)
             labels.append(label)
-
+        
     # Pad decoder_input_ids and labels
+
     decoder_input_ids = pad_sequences(decoder_input_ids, max_length=max_length, padding_value=tokenizer.pad_token_id)
     labels = pad_sequences(labels, max_length=max_length, padding_value=-100)
 
     return {
-        "input_ids": input_ids,
-        "attention_mask": attention_mask,
+        "input_ids": padded_input_datas,
+        "attention_mask": attention_masks,
         "decoder_input_ids": decoder_input_ids,
         "labels": labels
     }
 
 def filter_examples(example):
-    return len(example[f"encodec_0"]) <= 1000
+     return len(example[f"src_encodec_0"]) <= 1000 and len(example[f"tgt_encodec_0"]) <= 1000
 
 train_dataset = train_dataset.filter(filter_examples)
-valid_dataset = valid_dataset.filter(filter_examples)
+# valid_dataset = valid_dataset.filter(filter_examples)
 
 train_dataset = train_dataset.map(
     process_data_to_model_inputs,
-    remove_columns=train_dataset.column_names,
+    remove_columns=["src_encodec_2", "src_encodec_3", "src_encodec_4", "src_encodec_5", "src_encodec_6", "src_encodec_7",
+    "src_encodec_1", "tgt_encodec_0", "tgt_encodec_1", "tgt_encodec_2", "tgt_encodec_3", "tgt_encodec_4", "tgt_encodec_5",
+    "tgt_encodec_6", "tgt_encodec_7", "file_id", "instruction", "transcription", "src_encodec_0"],
     batched=True,
     batch_size=training_args.per_device_train_batch_size
 )
-valid_dataset = valid_dataset.map(process_data_to_model_inputs,
-                                  remove_columns=valid_dataset.column_names,
-                                  batched=True,
-                                  batch_size=training_args.per_device_eval_batch_size
-                                  )
+print(train_dataset)
+# valid_dataset = valid_dataset.map(process_data_to_model_inputs,
+#                                   remove_columns=valid_dataset.column_names,
+#                                   batched=True,
+#                                   batch_size=training_args.per_device_eval_batch_size
+#                                   )
 
 def compute_metrics(eval_pred):
     logits, labels = eval_pred
@@ -116,8 +164,8 @@ def compute_metrics(eval_pred):
 trainer = Seq2SeqTrainer(
     model=model,
     args=training_args,
-    train_dataset=train_dataset,
-    eval_dataset=valid_dataset,
+    train_dataset=train_dataset["train"],
+    # eval_dataset=valid_dataset,
     data_collator=data_collator,
     tokenizer=tokenizer,
     #compute_metrics=compute_metrics,
@@ -125,4 +173,4 @@ trainer = Seq2SeqTrainer(
 
 # Start training
 trainer.train()
-trainer.evaluate()
+# trainer.evaluate()
